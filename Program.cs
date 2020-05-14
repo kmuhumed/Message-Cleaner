5e78dde75082d41a3cee21244da22472c30822cc
Khadijo Muhumed <49961460+kmuhumed@users.noreply.github.com>
2020-05-14 01:03:16 -0600

    Add files

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a list of words to remove");
+            //Console.Write(System.Environment.NewLine);
+            Console.WriteLine();
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+
+                List<string> userlist = new List<string>();
+                userlist.Add(input);
+                string inputNewuser = Console.ReadLine();
+                userlist.Add(inputNewuser);
+                string userinput = Console.ReadLine();
+                //var button = Console.ReadKey();
+
+               
+            }
+            
+            
+                
+                
+                    
+                
+
+            }
+           
+        }
+    }
+
+
+    
+           
+         
+            
+        
+           
+    
+
